0b333bac52365a89fb956e2d26a80969858fd309
skjalf <padawanskjalf@gmail.com>
2013-07-08 14:14:01 -0700

    updating files

--- js/merge.js
+++ js/merge.js
@@ -1,3 +1,12 @@
 $(function() {
+  $.vegas('slideshow', {
+    backgrounds: [
+      {src: '/img/WebsiteBanner.jpg', valign: 'center'},
+      {src: '/img/cloud3.png', valign: 'top'}
+    ],
+    preload: true
+  })
+  ('overlay', {src: '/js/vegas/overlays/13.png'});
+
   $('.navbar').scrollspy();
 });
